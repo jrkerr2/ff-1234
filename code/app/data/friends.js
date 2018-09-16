3ac65074b02a64e8929b5a11b9b750ddbca6477e
jrkerr2 <johnrkerr@hotmail.com>
2018-09-16 13:27:41 -0400

    questions & answers objects added

--- code/app/data/friends.js
+++ code/app/data/friends.js
@@ -1,7 +1,7 @@
 var dataArray = [
     {
-        "name":"Ahmed",
-        "photo":"file://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name":"Bobby Douglass",
+        "photo":"file://code/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
             5,
             1,
---
+++ code/app/data/questions.js
@@ -0,0 +1,45 @@
+var questionArray = [
+    {
+        "label": "Question 1",
+        "text": "Do you consider yourself religious?",
+
+        "label": "Question 2",
+        "text": "Do you consider yourself new age / spiritual?",
+
+        "label": "Question 3",
+        "text": "Do you consider yourself conservative / right-wing, politically?",
+
+        "label": "Question 4",
+        "text": "Do you consider yourself liberal / left-wing, politically?",
+
+        "label": "Question 5",
+        "text": "Are you engergized by social interactions in large groups?",
+
+        "label": "Question 6",
+        "text": "Is your age above 40?",
+
+        "label": "Question 7",
+        "text": "Do you have a post-HS degree?",
+
+        "label": "Question 8",
+        "text": "Are you an animal lover?",
+
+        "label": "Question 9",
+        "text": "Do you have children?",
+
+        "label": "Question 10",
+        "text": "Are you a morning person?"
+        
+      }
+]
+
+var answerArray = [
+    {
+        "5": "(Strongly Agree)",
+        "4": "(Agree)",
+        "3": "(Neutral)",
+        "2": "(Disagree)",
+        "1": "(Strongly Disagree)"
+
+      }
+]
