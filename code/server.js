50de0b4e6b669cf2f0bf76418b2c5c4069831591
jrkerr2 <johnrkerr@hotmail.com>
2018-09-17 10:16:11 -0400

    init server working commit

--- code/server.js
+++ code/server.js
@@ -1,52 +1,15 @@
 // Dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-
-// Routes
-app.get("/", function(req, res) {
-  res.send("Welcome to the Star Wars Page!");
-});
-
-// Displays all characters
-app.get("/api/characters", function(req, res) {
-  return res.json(characters);
-});
-
-// Displays a single character, or shows "No character found"
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.send("No character found");
-
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  var newcharacter = req.body;
-
-  console.log(newcharacter);
-
-  characters.push(newcharacter);
-
-  res.json(newcharacter);
-});
-
+// listen on port 3000
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on express PORT " + PORT);
 });
