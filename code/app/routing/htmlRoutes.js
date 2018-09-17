73971c8f20a798838bc3ea1b3b7e374a4eab99a4
jrkerr2 <johnrkerr@hotmail.com>
2018-09-17 10:32:09 -0400

    2nd commit for html pages and routing

--- code/app/routing/htmlRoutes.js
+++ code/app/routing/htmlRoutes.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/survey.html'));
+});
+
+app.listen(3000);
