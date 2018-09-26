5ec8fb9cb9d620b498e2034b2dd5c301e3e4f21c
jrkerr2 <johnrkerr@hotmail.com>
2018-09-26 13:57:51 -0400

    heroku static test

--- code/server.js
+++ code/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use(apiRoutes);
 app.use(htmlRoutes);
 
 app.use(express.static("app/data/images/"));
+// app.use(express.static(path.join(__dirname, '../app/data/images')));
 
 // listen on port 3000 (or Heroku-assigned port)
 app.listen(PORT, function() {
